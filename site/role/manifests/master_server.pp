02111dc81dbe14218f607b8fd905e9adf0a54739
gerbeck <geoff.erbeck@emc.com>
2018-09-14 10:22:06 -0700

    Update master_server.pp

--- site/role/manifests/master_server.pp
+++ site/role/manifests/master_server.pp
@@ -1,4 +1,11 @@
-class role::master_server {
-  include profile::base
-  include profile::agent_nodes
-  }
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Sample Web Page</title>
+<META http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<body bgcolor="#ffffff" text="#000000">
+Is this working?
+
+</body>
+</html>
